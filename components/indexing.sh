4b12f9f6c1a1767bde5f465971094126954f1e03
ironmaniiith <aalekhj2507@gmail.com>
2016-05-13 21:53:11 +0530

    minor changes in indexing.sh

--- components/indexing.sh
+++ components/indexing.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 echo 'Enter the name of the file in which your set of alphabets are stored'
 read alphabets
 echo 'Now enter the name of the file in which you set of numbers are stored'
 read numbers
 
+# Alphabets
 alphabetLines=`cat $alphabets | wc -l`
 for i in `seq 1 8 $alphabetLines `;do
 	for j in `seq 0 6`; do
@@ -10,11 +12,14 @@ for i in `seq 1 8 $alphabetLines `;do
 	done | python parse.py
 done > finalIndexing.py
 
-numberLines=`cat $numbers | wc -l`
 
+# Numbers
+numberLines=`cat $numbers | wc -l`
 for i in `seq 1 8 $numberLines`; do
 	for j in `seq 0 6`; do
 		cat $numbers | awk NR==$(($i+$j))
 	done | python parse.py
 done >> finalIndexing.py
+
+# The final output looks something similar to what is stored in testIndexing.py
 echo 'Cool, now you have your final indexing in the file finalIndexing.py which will automatically be used'
